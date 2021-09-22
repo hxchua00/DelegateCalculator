354b3d159c77215c574f0853b63bc0ac5af40e33
hxchua00 <hxchua00@gmail.com>
2021-09-22 14:44:15 +0800

    Add project files.

---
+++ DelegateCalculator/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCalculator
+{
+    
+    class Calculator
+    {
+        int x, y;
+       
+        public Calculator(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Add(int x, int y)
+        {
+            Console.WriteLine("I'm in Calculator class, Add function.");
+            Console.WriteLine("Addition: " + (x + y));
+            Console.WriteLine();
+        }
+        public void Subtract(int x, int y)
+        {
+            Console.WriteLine("I'm in Calculator class, Subtract function.");
+            Console.WriteLine("Subtraction: " + (x - y));
+            Console.WriteLine();
+        }
+        public void Multiply(int x, int y)
+        {
+            Console.WriteLine("I'm in Calculator class, Multiply function.");
+            Console.WriteLine("Multiplication: " + (x * y));
+            Console.WriteLine();
+        }
+        public void Divide(int x, int y)
+        {
+            Console.WriteLine("I'm in Calculator class, Divide function.");
+            Console.WriteLine("Division: " + (x / y));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DelegateCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCalculator
+{
+    
+   
+    class Program
+    {
+        delegate void DelegateCal(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Calculator cal = new Calculator(x, y);
+            DelegateCal newOps = cal.Add;
+            newOps += cal.Subtract;
+            newOps += cal.Multiply;
+            newOps += cal.Divide;
+
+            newOps.Invoke(x, y);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
